e9bfc2d3d6b3921410db2b3985f3a91a63dbde0d
Debolina <moulisaha123@gmail.com>
2021-08-19 21:12:29 +0530

    --

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Debolina Saha',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/dfhcqiicv/image/upload/v1624199605/debo_prbkmv.jpg',
+  },
+  {
+    id: 2,
+    name: 'Mahima Soni',
+    age: 20,
+    image:
+      'https://res.cloudinary.com/dfhcqiicv/image/upload/v1624199585/mahima_yaerkv.jpg',
+  },
+  {
+    id: 3,
+    name: 'Aditi Sharma ',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/dfhcqiicv/image/upload/v1624199596/aditi_dpsdax.jpg',
+  },
+  {
+    id: 4,
+    name: 'Nidhi Patidar',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/dfhcqiicv/image/upload/v1624199629/nidhi_s7mn95.jpg',
+  },
+  {
+    id: 5,
+    name: 'Tanvi Garg',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/dfhcqiicv/image/upload/v1624199639/tanvi_nbfvcy.jpg',
+  },
+];
